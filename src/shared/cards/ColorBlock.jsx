956feb7d0ce9a5813e5c4deafc9e4d74a6d2c846
akshit-bansal11 <artistbansal2004@gmail.com>
2025-09-08 23:14:12 +0530

    Add Files

---
+++ src/shared/cards/ColorBlock.jsx
@@ -0,0 +1,26 @@
+//--------------------|    DEPENDENCIES    |--------------------//
+import React from "react";
+import { motion } from "framer-motion";
+
+
+//--------------------|     COMPONENT/S     |--------------------//
+import CopyButton from "../buttons/CopyButton";
+
+
+//--------------------|    MAIN RENDER     |--------------------//
+export default function ColorBlock({ color, className, iconSize, hover }) {
+
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            whileHover={hover}
+            className={`relative aspect-square overflow-hidden cursor-pointer group 
+                ${className}
+            `}
+            style={{ backgroundColor: color }}
+        >
+            <CopyButton text={color} iconSize={iconSize} isHover />
+        </motion.div>
+    );
+}
